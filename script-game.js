78cbff40d66f0f4ea3bf15994b4b0f397b494393
Justyna <97984841+JustynaBuczynska@users.noreply.github.com>
2022-03-02 22:44:42 +0100

    working game; no-finished

--- script-game.js
+++ script-game.js
@@ -5,23 +5,50 @@ function computerChoice() {
     return option[optionNumber];
 }
 
-function myChoice(message='Rock, paper or scissors? Choose one!') {
-    let userChoice = prompt(message).toLowerCase();
-
-    if (option.includes(userChoice)) {
-        return userChoice;
-    } else {
-        alert('Wrong input!');
-        return myChoice('WRONG! Try again! Rock, paper or scissors? Choose one!');
+let winComp=0;
+let winUser=0;
+function compare(choice) {
+    let user = choice;
+    let computer = computerChoice();
+    if (computer == user) { 
+        return 'Computer chose ' + computer + '.  User chose ' + user + '.  DRAW!' }
+    else if (computer == 'rock' && user == 'scissors' || computer == 'scissors' && user == 'paper' || computer == 'paper' && user == 'rock') {
+        winComp++;
+        return 'Computer chose ' + computer + '.  User chose ' +
+         user + '.  Computer win! Computer = ' + winComp + ' User = ' + winUser;
     }
+    else { 
+        winUser++;
+        return 'Computer chose ' + computer + '. User chose ' + user + '.  User win! Computer = ' + winComp + ' User = ' + winUser; }
 }
 
-function compare(){
-   let user=myChoice();
-   let computer=computerChoice();
-if(computer == user)
-{return 'Computer chose '+computer+'.  User chose ' +user+ '.  DRAW!'}
-else if (computer=='rock' && user=='scissors' || computer=='scissors' && user=='paper' || computer=='paper' && user=='rock' ){
-    return 'Computer chose '+computer+'.  User chose ' +user+ '.  Computer win!'}
-else { return 'Computer chose '+computer+'. User chose ' +user+ '.  User win!'}
+
+
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+    let result = compare('rock');
+    viewResults(result);
+});
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+
+    let result = compare('paper');
+    viewResults(result);
+});
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+
+    let result = compare('scissors');
+    viewResults(result);
+
+});
+
+function viewResults(message) {
+    const container = document.querySelector('#container');
+    const results = document.createElement('div');
+    results.classList.add('results');
+    results.textContent = message;
+    container.appendChild(results);
 }
