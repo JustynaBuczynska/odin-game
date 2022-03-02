0b60dfe2fba279a18b6cb44f3346135bc4b32932
Justyna <97984841+JustynaBuczynska@users.noreply.github.com>
2022-03-02 22:59:06 +0100

    working game; finished

--- script-game.js
+++ script-game.js
@@ -5,21 +5,23 @@ function computerChoice() {
     return option[optionNumber];
 }
 
-let winComp=0;
-let winUser=0;
+let winComp = 0;
+let winUser = 0;
 function compare(choice) {
     let user = choice;
     let computer = computerChoice();
-    if (computer == user) { 
-        return 'Computer chose ' + computer + '.  User chose ' + user + '.  DRAW!' }
+    if (computer == user) {
+        return 'Computer chose ' + computer + '.  User chose ' + user + '.  DRAW! Computer = ' + winComp + ' User = ' + winUser;
+    }
     else if (computer == 'rock' && user == 'scissors' || computer == 'scissors' && user == 'paper' || computer == 'paper' && user == 'rock') {
         winComp++;
         return 'Computer chose ' + computer + '.  User chose ' +
-         user + '.  Computer win! Computer = ' + winComp + ' User = ' + winUser;
+            user + '.  Computer win! Computer = ' + winComp + ' User = ' + winUser;
     }
-    else { 
+    else {
         winUser++;
-        return 'Computer chose ' + computer + '. User chose ' + user + '.  User win! Computer = ' + winComp + ' User = ' + winUser; }
+        return 'Computer chose ' + computer + '. User chose ' + user + '.  User win! Computer = ' + winComp + ' User = ' + winUser;
+    }
 }
 
 
@@ -28,6 +30,7 @@ const btn1 = document.querySelector('#btn1');
 btn1.addEventListener('click', () => {
     let result = compare('rock');
     viewResults(result);
+    endGame();
 });
 
 const btn2 = document.querySelector('#btn2');
@@ -35,6 +38,7 @@ btn2.addEventListener('click', () => {
 
     let result = compare('paper');
     viewResults(result);
+    endGame();
 });
 
 const btn3 = document.querySelector('#btn3');
@@ -42,13 +46,26 @@ btn3.addEventListener('click', () => {
 
     let result = compare('scissors');
     viewResults(result);
+    endGame();
 
 });
 
 function viewResults(message) {
     const container = document.querySelector('#container');
-    const results = document.createElement('div');
-    results.classList.add('results');
-    results.textContent = message;
-    container.appendChild(results);
+    container.textContent = message;
+
+}
+
+function endGame() {
+    if (winComp == 5) {
+        alert('END GAME. Computer WON!');
+        winComp=0;
+        winUser=0;
+        viewResults("");
+    } else if (winUser == 5){
+        alert('END GAME. User WON!');
+        winComp=0;
+        winUser=0;
+        viewResults("");
+    }
 }
